0a38fcb4111c1623d6faf40dacd59f844fea1670
Ahuja <dheeraj.ahuja@sap.com>
2020-09-06 17:37:41 +0530

    Props added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,34 +2,40 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   render() {
-    let classes = this.getBadgeClasses();
-
     return (
       <React.Fragment>
-        <span className={classes}>{this.getCount()}</span>
-        <button
-          className="btn btn-secondary btn-sum"
-         
-        >
-          Increment
-        </button>
+        <ul>
+          <span className={this.getBadgeClasses()}>{this.getCount()}</span>
+          <button
+            onClick={this.handleButtonClick}
+            className="btn btn-secondary btn-sum"
+          >
+            Increment
+          </button>
+        </ul>
       </React.Fragment>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge  m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   getCount() {
-    return this.state.count === 0 ? "ZERO" : this.state.count;
+    return this.state.value === 0 ? "ZERO" : this.state.value;
   }
+
+  handleButtonClick = () => {
+   this.setState({count :this.state.value+1});
+  };
+
 
 
 }
